b560bf086f03369f08ff70a360ac4464a9eaafc9
Alex Tsang <alextsang@live.com>
2020-10-10 17:53:39 +0900

    Code formatting

--- src/main/java/jp/co/axa/apidemo/entities/Employee.java
+++ src/main/java/jp/co/axa/apidemo/entities/Employee.java
@@ -11,28 +11,28 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="EMPLOYEE")
+@Table(name = "EMPLOYEE")
 public class Employee {
 
-    @Getter
-    @Setter
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long id;
+  @Getter
+  @Setter
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Getter
-    @Setter
-    @Column(name="EMPLOYEE_NAME")
-    private String name;
+  @Getter
+  @Setter
+  @Column(name = "EMPLOYEE_NAME")
+  private String name;
 
-    @Getter
-    @Setter
-    @Column(name="EMPLOYEE_SALARY")
-    private Integer salary;
+  @Getter
+  @Setter
+  @Column(name = "EMPLOYEE_SALARY")
+  private Integer salary;
 
-    @Getter
-    @Setter
-    @Column(name="DEPARTMENT")
-    private String department;
+  @Getter
+  @Setter
+  @Column(name = "DEPARTMENT")
+  private String department;
 
 }
