af1a41850d7251393ce5f096887d7b834dec26be
Alex Tsang <alextsang@live.com>
2020-10-11 03:47:12 +0900

    Enable caching
    Use caching to speed up response time.

--- src/main/java/jp/co/axa/apidemo/repositories/EmployeeRepository.java
+++ src/main/java/jp/co/axa/apidemo/repositories/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package jp.co.axa.apidemo.repositories;
 
-import jp.co.axa.apidemo.entities.Employee;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import jp.co.axa.apidemo.entities.Employee;
+
 @Repository
+@Cacheable(cacheNames = "employeeCache")
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
