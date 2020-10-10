04db22ee722f47301eaa4c9afe8ed0c32b98fab9
Alex Tsang <alextsang@live.com>
2020-10-11 03:52:25 +0900

    Code formatting

--- src/main/java/jp/co/axa/apidemo/entities/Employee.java
+++ src/main/java/jp/co/axa/apidemo/entities/Employee.java
@@ -1,14 +1,14 @@
 package jp.co.axa.apidemo.entities;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "EMPLOYEE")
