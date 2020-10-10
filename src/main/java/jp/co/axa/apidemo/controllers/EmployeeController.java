e9e418155a6f6833677d2b87025c4d2a6d5bed00
Alex Tsang <alextsang@live.com>
2020-10-11 03:52:12 +0900

    Check record when getting, updating, or deleting employee
    Check whether the target employee exists before altering the record, instead of
    silently accepting the request.

--- src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
+++ src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
@@ -1,11 +1,22 @@
 package jp.co.axa.apidemo.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import jp.co.axa.apidemo.entities.Employee;
 import jp.co.axa.apidemo.services.EmployeeService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -25,9 +36,13 @@ public class EmployeeController {
   }
 
   @GetMapping("/employees/{employeeId}")
-  public Employee getEmployee(
+  public ResponseEntity<Employee> getEmployee(
       @PathVariable(name = "employeeId") Long employeeId) {
-    return employeeService.getEmployee(employeeId);
+    Optional<Employee> optEmployee = employeeService.getEmployee(employeeId);
+    if (optEmployee.isEmpty()) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+    return new ResponseEntity<Employee>(optEmployee.get(), HttpStatus.OK);
   }
 
   @PostMapping("/employees")
@@ -37,21 +52,27 @@ public class EmployeeController {
   }
 
   @DeleteMapping("/employees/{employeeId}")
-  public void deleteEmployee(
+  public ResponseEntity<String> deleteEmployee(
       @PathVariable(name = "employeeId") Long employeeId) {
+    Optional<Employee> optEmployee = employeeService.getEmployee(employeeId);
+    if (optEmployee.isEmpty()) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
     employeeService.deleteEmployee(employeeId);
     System.out.println("Employee Deleted Successfully");
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 
   @PutMapping("/employees/{employeeId}")
-  public void updateEmployee(
+  public ResponseEntity<String> updateEmployee(
       @RequestBody Employee employee,
       @PathVariable(name = "employeeId") Long employeeId) {
-    Employee emp = employeeService.getEmployee(employeeId);
-    if (emp != null) {
-      employeeService.updateEmployee(employee);
+    Optional<Employee> optEmployee = employeeService.getEmployee(employeeId);
+    if (optEmployee.isEmpty()) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-
+    employeeService.updateEmployee(employee);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
   }
 
 }
--- src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
+++ src/main/java/jp/co/axa/apidemo/services/EmployeeServiceImpl.java
@@ -1,12 +1,13 @@
 package jp.co.axa.apidemo.services;
 
-import jp.co.axa.apidemo.entities.Employee;
-import jp.co.axa.apidemo.repositories.EmployeeRepository;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import jp.co.axa.apidemo.entities.Employee;
+import jp.co.axa.apidemo.repositories.EmployeeRepository;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -23,9 +24,8 @@ public class EmployeeServiceImpl implements EmployeeService {
     return employees;
   }
 
-  public Employee getEmployee(Long employeeId) {
-    Optional<Employee> optEmp = employeeRepository.findById(employeeId);
-    return optEmp.get();
+  public Optional<Employee> getEmployee(Long employeeId) {
+    return employeeRepository.findById(employeeId);
   }
 
   public void saveEmployee(Employee employee) {
